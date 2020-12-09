b6753e9522cb994369513616551ac07c9e164515
YogeshKanda <kanda.yogesh@gmail.com>
2020-12-10 00:12:25 +0530

    header component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     //BEM naming convention
     <div className="app">
-      <h1>FB clone</h1>
       <Header />
       {
         /* App body */
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,16 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleOutlinedIcon from "@material-ui/icons/SupervisedUserCircleOutlined";
+import AddIcon from "@material-ui/icons/Add";
+import { Avatar, IconButton } from "@material-ui/core";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Header() {
   return (
@@ -9,9 +20,46 @@ function Header() {
           src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Akash_rajoriya_Facebook_.png"
           alt="logo"
         />
+        <div className="header_input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
-      <div className="header_middle"></div>
-      <div className="header_right"></div>
+      <div className="header_center">
+        <div className="header_option header_option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircleOutlinedIcon fontSize="large" />
+        </div>
+      </div>
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>Your Name</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
     </div>
   );
 }
